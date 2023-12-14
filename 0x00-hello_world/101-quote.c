a09793a0cd4833f73bd7c8cac30c7efb936731db
jephdon <chukwujekwujephthah@gmail.com>
2023-12-14 03:56:05 -0800

    Unix simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - print to standard error
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
