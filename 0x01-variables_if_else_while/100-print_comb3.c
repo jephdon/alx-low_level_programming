f346aad876ab7840c7b2f1c9504706e135c54fb0
jephdon <chukwujekwujephthah@gmail.com>
2024-07-20 04:15:24 -0700

    All possible combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+	int j;
+
+	while (i < 10)
+	{
+		j = i + 1;
+		while (j < 10)
+		{
+			putchar('0' + i);
+			putchar('0' + j);
+			if (i < 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
