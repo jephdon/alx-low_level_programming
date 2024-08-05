426bf6d78dc513a69e0e63af3e726c6cf54bbc2c
jephdon <chukwujekwujephthah@gmail.com>
2024-08-05 10:16:32 -0700

    Print the second half of a string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,10 +10,16 @@ void puts_half(char *str)
 {
 	int i = 0, length = 0;
 
+	/* Calculate the length of string*/
 	while (str[length] != '\0')
 	{
 		length++;
 	}
+	/**
+	 * Printing from half of the string
+	 * put into consideration strings with
+	 * odd numbers
+	 */
 	for (i = length / 2; i <= length - 1; i++)
 	{
 		_putchar(str[i]);
