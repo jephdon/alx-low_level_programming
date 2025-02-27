dd1a84eb27603b4422385dd03353feecab1f47e9
jephdon <chukwujekwujephthah@gmail.com>
2025-02-27 01:15:30 -0800

    Print all lowercase alphabet except q anf e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		if (alpha != 'q' && alpha != 'e')
+			putchar(alpha);
+		alpha++;
+	}
+	putchar('\n');
+	return (0);
+}
