0e96be4ffac176c146620c2fc0a14894c1f3dee8
jephdon <chukwujekwujephthah@gmail.com>
2025-03-10 14:12:59 -0700

    Program prints the number argument passed into it

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	int i = 0, count = 0;
+
+	while (i < argc)
+	{
+		count++;
+		i++;
+	}
+	printf("%d\n", count);
+	return (0);
+}
