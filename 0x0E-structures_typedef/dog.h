a4ae74761801b6ae9b5d193fb654b1daa152dca2
jephdon <chukwujekwujephthah@gmail.com>
2025-03-18 12:27:38 -0700

    Defined Struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Collects details of dogs
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ *
+ * Description: This struct tries to collect details on dogs
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* DOG_H */
