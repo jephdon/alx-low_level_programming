d9abfbf549e26b53d8d351627e0a597117e8ad85
jephdon <chukwujekwujephthah@gmail.com>
2025-03-28 15:19:01 -0700

    Function prints anything

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,93 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_char - prints char types
+ * @ap: parameter
+ *
+ * Return: nothing
+ */
+void print_char(va_list ap)
+{
+	printf("%c", va_arg(ap, int));
+}
+
+/**
+ * print_int - prints int types
+ * @ap: parameter
+ *
+ * Return: nothing
+ */
+void print_int(va_list ap)
+{
+	printf("%d", va_arg(ap, int));
+}
+
+/**
+ * print_float - prints float types
+ * @ap: parameter
+ *
+ * Return: nothing
+ */
+void print_float(va_list ap)
+{
+	printf("%f", va_arg(ap, double));
+}
+
+/**
+ * print_string - print string types
+ * @ap: parameter
+ *
+ * Return: nothing
+ */
+void print_string(va_list ap)
+{
+	char *s = va_arg(ap, char *);
+
+	if (s == NULL)
+		printf("(nil)");
+	else
+		printf("%s", s);
+}
+
+/**
+ * print_all - prints anything
+ * @format: parameter type variable
+ *
+ * Return: nothing
+ */
+void print_all(const char * const format, ...)
+{
+	format_type printers[] = {
+		{'c', print_char},
+		{'i', print_int},
+		{'f', print_float},
+		{'s', print_string},
+		{0, NULL}
+	};
+	va_list ap;
+	const char *fmt = format;
+	int i, first = 1;
+
+	va_start(ap, format);
+	while (*fmt)
+	{
+		i = 0;
+		while (printers[i].type != 0)
+		{
+			if (*fmt == printers[i].type)
+			{
+				if (!first)
+					printf(", ");
+				printers[i].print_func(ap);
+				first = 0;
+				break;
+			}
+			i++;
+		}
+		fmt++;
+	}
+	va_end(ap);
+	printf("\n");
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,10 +2,16 @@
 #define VARIADIC_FUNCTIONS_H
 
 #include <stddef.h>
+#include <stdarg.h>
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+typedef struct format {
+	char type;
+	void (*print_func)(va_list);
+}format_type;
 
 #endif /* VARIADIC_FUNCTIONS_H */
