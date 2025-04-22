dd78a30255c646f0a420d463b189f8370a30ad51
jephdon <chukwujekwujephthah@gmail.com>
2025-04-21 18:37:14 -0700

    Function prints the binary representation of a number

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: number to print in binary
+ *
+ * Return: nothing
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int mask;
+	int flag = 0; /* tracks when we start printing to avoid leading zeroes */
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	/* starts mask at the highest bit (bit 63 for unsigned long int) */
+	mask = 1UL << 62;
+	while (mask > 0)
+	{
+		if (n & mask)
+		{
+			_putchar('1');
+			flag = 1;
+		}
+		else if (flag)
+		{
+			_putchar('0');
+		}
+		mask = mask >> 1; /* shifts mask right to check the next bit */
+	}
+}
